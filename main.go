3917775d560525d9a4f87784e0e265351cd57d19
ahmad fauzan <fauzan.c4@gmail.com>
2022-04-09 02:34:55 +0700

    get parameter id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -11,6 +13,9 @@ func main() {
 	// route
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/product", productHandler)
+
+	log.Println("Starting web on port 8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
@@ -27,4 +32,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world, saya sedang belajar golang web"))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	dataId, err := strconv.Atoi(id)
+
+	if err != nil || dataId < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Product page : %d", dataId)
 }
