a844215989771159a4fd3b52822891324d0aef9d
ahmad fauzan <fauzan.c4@gmail.com>
2022-04-09 02:06:26 +0700

    create route

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func main() {
 	mux := http.NewServeMux()
 
 	// route
+	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Home page"))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
