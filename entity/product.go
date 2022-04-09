1fc8f357e703cc0e34b74b467b513321f7d65b4f
ahmad fauzan <fauzan.c4@gmail.com>
2022-04-10 03:22:58 +0700

    create function

--- entity/product.go
+++ entity/product.go
@@ -6,3 +6,13 @@ type Product struct {
 	Price int
 	Stock int
 }
+
+func (product Product) StockStatus() string {
+	var status string
+	if product.Stock < 5 {
+		status = "Stok hampir habis"
+	} else if product.Stock < 10 {
+		status = "Stok terbatas"
+	}
+	return status
+}
